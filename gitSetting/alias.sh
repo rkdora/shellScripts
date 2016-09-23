3a23353faa75e4bb02a88ed24c42d20d305536c9
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-09-23 17:21:20 +0900

    Delete alias.sh
    Alias move to dotfiles

--- gitSetting/alias.sh
+++
@@ -1,9 +0,0 @@
-git config --global alias.aa "add -A"
-git config --global alias.c commit
-git config --global alias.ca "commit -a"
-git config --global alias.co checkout
-git config --global alias.cob "checkout -b"
-git config --global alias.st status
-git config --global alias.graph "log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'"
-git config --global alias.gr "log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'"
-
