c24343e89cbbcebbbdae3ea2eba039109ab88529
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-10-18 23:12:12 +0900

    Update gcc-7 installation

--- installer/compiler/gcc/install.sh
+++ installer/compiler/gcc/install.sh
@@ -3,6 +3,5 @@ sudo apt update
 sudo apt install -y g++-7
 sudo apt upgrade -y
 sudo apt autoremove -y
-echo "export CXX=g++-7 CC=gcc-7" >> ~/.bashrc
-. ~/.bashrc
-echo $CXX
+sudo ln -s /usr/bin/gcc-7 /usr/local/bin/gcc
+sudo ln -s /usr/bin/g++-7 /usr/local/bin/g++
